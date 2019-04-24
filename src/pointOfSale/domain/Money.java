8e11c75017ed8a1e2e6b3c827af2efe2c9832c22
Zandros229 <ernest.figura548@gmail.com>
2019-04-24 11:05:04 +0200

    Add Money class

---
+++ src/pointOfSale/domain/Money.java
@@ -0,0 +1,31 @@
+package pointOfSale.domain;
+
+import java.util.Objects;
+
+public class Money {
+    private Double amount;
+    private String courency;
+
+    public Money(Double amount, String courency) {
+        this.amount = amount;
+        this.courency = courency;
+    }
+
+    public Double getAmount() {
+        return amount;
+    }
+
+    public String getCourency() {
+        return courency;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Money)) return false;
+        Money money = (Money) o;
+        return Objects.equals(getAmount(), money.getAmount()) &&
+                Objects.equals(getCourency(), money.getCourency());
+    }
+    
+}
