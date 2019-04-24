efb2b56a7d446f263f156954e9c9ff0a6b7c4d89
Zandros229 <ernest.figura548@gmail.com>
2019-04-24 11:17:00 +0200

    Add BarCodeScanner class

--- src/pointOfSale/InputDevice/BarCodeScanner.java
+++ src/pointOfSale/InputDevice/BarCodeScanner.java
@@ -1,4 +1,14 @@
 package pointOfSale.InputDevice;
 
 public class BarCodeScanner {
+    private String barCode;
+
+    public int scan(String scannedBarCode){
+        barCode=scannedBarCode;
+        return barCodeToId();
+    }
+
+    private int barCodeToId(){
+        return Integer.parseInt(barCode);
+    }
 }
